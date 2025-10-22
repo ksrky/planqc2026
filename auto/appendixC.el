427bb5ae3f09bab8aa4d89d0849f9336f384fc6a
ksrky <xgrk9475@gmail.com>
2025-10-23 03:05:46 +0900

    wip

---
+++ auto/appendixC.el
@@ -0,0 +1,10 @@
+;; -*- lexical-binding: t; -*-
+
+(TeX-add-style-hook
+ "appendixC"
+ (lambda ()
+   (LaTeX-add-labels
+    "sec:type-system"
+    "fig:typing"))
+ :latex)
+
---
+++ auto/section3.3.el
@@ -0,0 +1,11 @@
+;; -*- lexical-binding: t; -*-
+
+(TeX-add-style-hook
+ "section3.3"
+ (lambda ()
+   (LaTeX-add-labels
+    "def:single-step"
+    "fig:reduction"
+    "thm:norm-pres"))
+ :latex)
+
